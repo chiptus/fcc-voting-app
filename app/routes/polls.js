1f26d2b7fcd5b790742e4b0b0cc7d0d96b815d53
chiptus <chiptus@gmail.com>
2017-02-21 17:13:25 +0200

    add routes for existing commands in controller

--- app/routes/polls.js
+++ app/routes/polls.js
@@ -1,12 +1,47 @@
-const Poll = require('../models/poll');
+const PollCtrl = require('../controllers/poll.controller');
 
 module.exports = (app) => {
 
 	app.route('/api/polls')
 		.get((req, res) => {
 			Poll.find({})
-        .exec()
-        .then(result => res.json(result))
-        .catch(reason => res.send({error: reason}));
-		})
+				.exec()
+				.then(result => res.json(result))
+				.catch(reason => res.send({ error: reason }));
+		});
+
+	app.post('/api/create-poll', ({body}, res) => {
+		if (!body.name) {
+			return res.json({ error: 'name isn\'t supplied' });
+		}
+		return PollCtrl.createPoll(body.name, body.options)
+			.then(document => res.json(document),
+			reason => res.json({ error: reason }));
+	});
+
+	app.post('/api/poll/:id/vote/:optionId', ({params: {id, optionId }}, res) => {
+		if (!id) {
+			return res.json({ error: 'id isn\'t supplied' });
+		}
+		if (!optionId) {
+			return res.json({ error: 'option id isn\'t supplied' });
+		}
+		return PollCtrl.voteForPoll(id, optionId)
+			.then(
+				() => res.json({ success: true }),
+				reason => res.json({ error: reason })
+			);
+	});
+
+	app.get('/api/poll/:id', ({params: {id}}, res) => {
+		if (!id) {
+			return res.json({ error: 'id isn\'t supplied' });
+		}
+		return PollCtrl.getPoll(id)
+			.then(
+				doc => res.json(doc),
+				reason => res.json({ error: reason })
+			);
+	});
+
 }
