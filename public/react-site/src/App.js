6393093501bcffe59d1fc49c97d78178d4f0eea9
chiptus <chiptus@gmail.com>
2017-02-27 10:47:29 +0200

    add footer-header to root

--- public/react-site/src/App.js
+++ public/react-site/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, /*NavLink */ } from 'react-router-dom';
 
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
 
+import { Header, Footer } from './components/layout';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
@@ -16,47 +14,28 @@ injectTapEventPlugin();
 
 import theme from './theme';
 
+import LoginPage from './auth/login-page/login-page';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      openDrawer: false,
-    }
   }
-
-  toggleDrawer = () => {
-    this.setState({ openDrawer: !this.state.openDrawer });
-  }
-
-  /*
-    <Drawer open={this.state.openDrawer} docked={false} onRequestChange={() => console.log('requres')}>
-        <MenuItem secondaryText={'X'} onTouchTap={this.toggleDrawer}></MenuItem>
-        <NavLink to="/courses" activeClassName="active"><MenuItem  onTouchTap={this.toggleDrawer}>מנות</MenuItem></NavLink>
-        <NavLink to="/menus" activeClassName="active"><MenuItem onTouchTap={this.toggleDrawer}>תפריטים</MenuItem></NavLink>
-        <NavLink to="/events" activeClassName="active"><MenuItem onTouchTap={this.toggleDrawer}>אירועים</MenuItem></NavLink>
-        <NavLink to="/ingredients" activeClassName="active"><MenuItem onTouchTap={this.toggleDrawer}>רכיבים</MenuItem></NavLink>
-  </Drawer>
-  */
 
   render() {
     return (
       <BrowserRouter >
         <MuiThemeProvider muiTheme={theme}>
-          <div>
 
-            <div className="container">
-              <AppBar title="Voty" /*onLeftIconButtonTouchTap={this.toggleDrawer}*/>
-
-              </AppBar>
-              <div className="content">
-                <Route path="/login" render={() => <div>not logged in</div>} />
-                
-              </div>
+          <div className="container">
+            <Header />
+            <div className="content">
+              <Route path="/login" component={LoginPage} />
+              <Route path="/test" component={"div"} />
             </div>
+            <Footer />
           </div>
+
         </MuiThemeProvider>
       </BrowserRouter>
     );
