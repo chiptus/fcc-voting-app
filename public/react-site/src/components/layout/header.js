6888b1871e9cd01e9f46dbfb243f82be8bfce406
chiptus <chiptus@gmail.com>
2017-02-27 10:45:31 +0200

    layout

---
+++ public/react-site/src/components/layout/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Header = () => {
+  return (
+    <AppBar title="Voty" /*onLeftIconButtonTouchTap={this.toggleDrawer}*/>
+    </AppBar>
+  );
+};
+
+export default Header;
