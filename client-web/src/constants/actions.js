0991c9cb64055c1380850baf6771a2c490f10170
chiptus <chiptus@gmail.com>
2017-02-28 10:04:08 +0200

    basic action constants

---
+++ client-web/src/constants/actions.js
@@ -0,0 +1,8 @@
+export const REQUEST_POLLS = 'REQUEST_POLLS';
+export const RECEIVED_POLLS = 'RECEIVED_POLLS';
+
+export const CREATE_POLL_REQUEST = 'CREATE_POLL_REQUEST';
+export const ADD_POLL = 'ADD_POLL';
+
+export const REQUEST_VOTE = 'REQUEST_VOTE';
+export const RESPONSE_VOTE = 'RESPONSE_VOTE';
