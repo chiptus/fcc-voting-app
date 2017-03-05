1869e1acae940aa71052f59904a216ebb4d027c9
chiptus <chiptus@gmail.com>
2017-03-05 09:41:15 +0200

    poll reducers

--- client-web/src/reducers/polls.js
+++ client-web/src/reducers/polls.js
@@ -4,8 +4,10 @@ export default function pollsReducer(state = [], {type, payload, error, meta}) {
   switch (type) {
     case ACTIONS.ADD_POLL:
       return [payload.id, ...state];
-    case ACTIONS.CREATE_POLL_REQUEST:
     case ACTIONS.RECEIVED_POLLS:
+      return payload.map(p => p.id);
+
+    case ACTIONS.CREATE_POLL_REQUEST:
     case ACTIONS.REQUEST_POLLS:
     case ACTIONS.REQUEST_VOTE:
     case ACTIONS.RESPONSE_VOTE:
@@ -14,12 +16,3 @@ export default function pollsReducer(state = [], {type, payload, error, meta}) {
   }
 }
 
-
-function addPoll(state, poll) {
-  if (state.auth.userId === poll.created_by_user_id) {
-    //change user object to contain a reference to the poll
-  }
-  //add poll to polls array,
-  // add options to options entity 
-  // add poll (normalized) to polls entity
-}
