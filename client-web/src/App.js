b7e17371553909fb255ca8d358322816411456bd
chiptus <chiptus@gmail.com>
2017-03-07 11:12:49 +0200

    root app - connect to redux

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, /*NavLink */ } from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import theme from './theme';
 
 import './App.css';
-
+import {getPolls} from './actions/polls-list';
 import { Header, Footer } from './components/layout';
 
 import LoginPage from './auth/login-page/login-page';
@@ -15,6 +16,10 @@ import { HomePage, MyPollsPage, NewPollPage, PollPage } from './containers';
 
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getPolls();
+  }
+
   render() {
     return (
       <BrowserRouter >
@@ -41,7 +46,15 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state,ownProps) => ({
+  
+});
+
+const mapDispatchToProps = (dispatch,ownProps) => ({
+  getPolls: () => dispatch(getPolls()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 
 function createOpenPollFunction(push) {
