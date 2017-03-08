ae2ddbeeba99ffe38b64c485660374e5b4401485
chiptus <chiptus@gmail.com>
2017-03-08 08:17:56 +0200

    poll entity reducer normalize options

--- client-web/src/reducers/entities/polls.js
+++ client-web/src/reducers/entities/polls.js
@@ -13,28 +13,13 @@ const defaultState = {};
 export default function entitiesPollsReducer(state = defaultState, { type, payload, error, meta }) {
   switch (type) {
     case ACTIONS.RECEIVED_POLLS:
-      return convertArrayToObject(payload.items);
+      return receivedPolls(payload.items);
     case ACTIONS.ADD_POLL:
       return {
         ...state,
         [payload.poll._id]: payload.poll,
       };
-    case ACTIONS.RESPONSE_VOTE:
-      return {
-        ...state,
-        [payload.pollId]: {
-          ...state[payload.pollId],
-          options: state[payload.pollId].options.map(o => {
-            if (o.id !== payload.optionId) {
-              return o;
-            }
-            return {
-              ...o,
-              value: o.value++
-            }
-          })
-        }
-      };
+      
     case ACTIONS.CREATE_OPTION:
       return {
         ...state,
@@ -46,12 +31,20 @@ export default function entitiesPollsReducer(state = defaultState, { type, paylo
           ]
         }
       }
+    case ACTIONS.RESPONSE_VOTE:
     case ACTIONS.CREATE_POLL_REQUEST:
     case ACTIONS.REQUEST_POLLS:
     case ACTIONS.REQUEST_VOTE:
     default:
       return state;
   }
+}
+
+function receivedPolls(polls = []) {
+  return convertArrayToObject(polls.map(poll => ({
+    ...poll,
+    options: poll.options.filter(o => o).map(o => o._id),
+  })));
 }
 
 // function normalizePolls(polls = []) {
