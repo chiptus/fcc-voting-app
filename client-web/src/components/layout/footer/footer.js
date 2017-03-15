e671211baa94b25c7bc692bb28058edaa131ac43
chiptus <chiptus@gmail.com>
2017-03-15 16:35:50 +0200

    footer

---
+++ client-web/src/components/layout/footer/footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './footer.css';
+
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle, FlatButton} from 'material-ui'
+
+const Footer = () => {
+  return (
+    
+      <Toolbar>
+        <ToolbarTitle text="Created by Chaim Lando" />
+
+        <ToolbarGroup>
+          <FlatButton label={<a href="http://www.github.com/chiptus">GitHub</a>} />
+          <ToolbarSeparator />
+          <FlatButton label={<a href="http://chiptus.github.com/">About</a>} />
+          <ToolbarSeparator />          
+          <FlatButton label={<a href="http://tdigitalnomad.blogspot.com">Blog</a>} />
+        </ToolbarGroup>
+      </Toolbar>
+    
+  );
+};
+
+export default Footer;
