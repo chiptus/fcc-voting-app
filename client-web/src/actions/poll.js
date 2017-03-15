cd6cc1d617b58bab6461d62195ffc7047af9d8b3
chiptus <chiptus@gmail.com>
2017-03-15 18:25:17 +0200

    create poll with axios wrapper and return userId

--- client-web/src/actions/poll.js
+++ client-web/src/actions/poll.js
@@ -2,15 +2,8 @@ import * as ACTIONS from '../constants/actions';
 import { SERVER_URL } from '../config';
 
 export function createPoll(poll) {
-  return dispatch => {
+  return dispatch =>
     dispatch(requestCreatePoll(poll))
-      .then(({ data }) => {
-        dispatch(addPoll(data))
-      })
-      .catch(error => {
-        dispatch(createPollFailed(error));
-      })
-  }
 }
 
 function requestCreatePoll(poll) {
@@ -21,17 +14,23 @@ function requestCreatePoll(poll) {
         url: `${SERVER_URL}/api/create-poll`,
         data: poll,
         method: 'post'
+      },
+      options: {
+        onSuccess: ({ dispatch, response }) => dispatch(addPoll(response.data.poll, response.data.userId)),
+        onError: ({ dispatch, error }) => dispatch(createPollFailed(error)),
       }
     }
   }
 }
 
-function addPoll(poll) {
+
+function addPoll(poll, userId) {
   return {
     type: ACTIONS.ADD_POLL,
     payload: {
       poll,
-      time: (new Date()).getTime()
+      time: (new Date()).getTime(),
+      userId,
     }
   }
 }
