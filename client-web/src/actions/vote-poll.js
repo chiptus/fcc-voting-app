74f67f97611c6bf23fd98f229d15be0cf56eab13
chiptus <chiptus@gmail.com>
2017-03-15 16:08:27 +0200

    vote-poll axios wrapper

--- client-web/src/actions/vote-poll.js
+++ client-web/src/actions/vote-poll.js
@@ -1,22 +1,19 @@
 import * as ACTIONS from '../constants/actions';
-import axios from 'axios';
-import {SERVER_URL} from '../config';
+
+import { SERVER_URL } from '../config';
 
 export default function voteForPoll(pollId, optionId) {
-  return dispatch => {
-    dispatch(requestVoteForPoll(pollId, optionId));
-    return axios.post(`${SERVER_URL}/api/poll/${pollId}/vote/${optionId}`)
-      .then(result => dispatch(voteSuccess(pollId, optionId)),
-        error => dispatch(voteError(error)));
-  }
-}
-
-function requestVoteForPoll(pollId, optionId) {
   return {
     type: ACTIONS.REQUEST_VOTE,
     payload: {
-      pollId,
-      optionId,
+      request: {
+        url: `${SERVER_URL}/api/poll/${pollId}/vote/${optionId}`,
+        method: 'post',
+      },
+      options: {
+        onSuccess: ({ dispatch, response }) => dispatch(voteSuccess(pollId, optionId)),
+        onError: ({ dispatch, error }) => dispatch(voteError(pollId, optionId)),
+      }
     }
   }
 }
