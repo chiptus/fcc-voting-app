8f917ab8690dd4f86015bcfee4b038f4f99548db
chiptus <chiptus@gmail.com>
2017-03-16 15:39:48 +0200

    social bar empty component

---
+++ client-web/src/components/poll-page/social-bar.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+
+const SocialBar = props => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+SocialBar.propTypes = {
+  
+};
+
+export default SocialBar;
