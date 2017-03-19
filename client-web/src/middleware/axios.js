fba8c21047cfc755642ffcc211264dd0d48c4b9a
chiptus <chiptus@gmail.com>
2017-03-19 16:27:54 +0200

    logout when 403 is returned

--- client-web/src/middleware/axios.js
+++ client-web/src/middleware/axios.js
@@ -1,26 +1,30 @@
-import axios from 'axios';
-import axiosMiddleware from 'redux-axios-middleware';
-import { authDiscardToken } from '../actions/auth';
-const client = axiosMiddleware(axios.create({
-}), {
-
-    interceptors: {
-      request: [({ getState }, config) => {
+import axios from "axios";
+import axiosMiddleware from "redux-axios-middleware";
+import { authDiscardToken } from "../actions/auth";
+const client = axiosMiddleware(axios.create({}), {
+  interceptors: {
+    request: [
+      ({ getState }, config) => {
         return {
           ...config,
           params: {
             ...config.params,
-            jwt: getState().auth.token,
+            jwt: getState().auth.token
           }
+        };
+      }
+    ],
+    response: [
+      {
+        error: ({ dispatch }, e) => {
+          if (e.response.status === 403) {
+            return dispatch(authDiscardToken());
+          }
+          return e;
         }
-      }],
-      response: [({ dispatch }, response) => {
-        if (response.status === 403) {
-          dispatch(authDiscardToken());
-        }
-        return response;
-      }]
-    }
-  })
+      }
+    ]
+  }
+});
 
 export default client;
