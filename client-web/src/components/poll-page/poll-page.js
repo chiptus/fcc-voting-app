71e3a61a67e4c67d41dcc00bbbf518275793b312
chiptus <chiptus@gmail.com>
2017-03-19 18:53:14 +0200

    options-list scrollbar

--- client-web/src/components/poll-page/poll-page.js
+++ client-web/src/components/poll-page/poll-page.js
@@ -1,12 +1,12 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component, PropTypes } from 'react';
 
-import Header from "./poll-header";
-import OptionsList from "./options-list";
-import VotingChart from "./voting-chart";
-import AddOption from "./add-option";
-import SocialBar from "./social-bar";
+import Header from './poll-header';
+import OptionsList from './options-list';
+import VotingChart from './voting-chart';
+import AddOption from './add-option';
+import SocialBar from './social-bar';
 
-import { Paper } from "material-ui";
+import { Paper } from 'material-ui';
 
 export default class PollPage extends Component {
   render() {
@@ -18,27 +18,25 @@ export default class PollPage extends Component {
     return (
       <Paper
         style={{
-          height: "calc(100% - 37px)",
-          width: "60%",
-          margin: "0 auto",
-          padding: "25px"
+          height: 'calc(100% - 37px)',
+          width: '60%',
+          margin: '0 auto',
+          padding: '25px',
         }}
-        zDepth={4}
-      >
-        <Header name={poll.name} author={poll.author || "Unknown"} />
+        zDepth={4}>
+        <Header name={poll.name} author={poll.author || 'Unknown'} />
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            height: "calc(100% - 88px)"
-          }}
-        >
-          <div style={{ flex: 1, height: "100%" }}>
+            display: 'flex',
+            flexDirection: 'column',
+            height: 'calc(100% - 88px)',
+          }}>
+          <div style={{ flex: 1, height: '100%', overflowY: 'auto' }}>
             <OptionsList options={options} vote={vote} />
             {isSignedIn &&
               <AddOption addOption={addOption} pollId={poll._id} />}
           </div>
-          <div style={{ flex: 1, height: "100%" }}>
+          <div style={{ flex: 1, height: '100%' }}>
             <VotingChart options={options} />
             <SocialBar poll={poll} />
           </div>
@@ -52,17 +50,17 @@ export default class PollPage extends Component {
 PollPage.propTypes = {
   poll: PropTypes.shape({
     name: PropTypes.string.isRequired,
-    author: PropTypes.string
+    author: PropTypes.string,
   }),
   options: PropTypes.arrayOf(
     PropTypes.shape({
       _id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
-      value: PropTypes.number
+      value: PropTypes.number,
     })
   ),
   vote: PropTypes.func.isRequired,
   isVotedFor: PropTypes.bool,
   addOption: PropTypes.func.isRequired,
-  isSignedIn: PropTypes.bool
+  isSignedIn: PropTypes.bool,
 };
