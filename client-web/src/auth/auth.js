d955506b5baf9d69123fd5199a7818b20bccff61
chiptus <chiptus@gmail.com>
2017-03-20 15:23:30 +0200

    env variables

--- client-web/src/auth/auth.js
+++ client-web/src/auth/auth.js
@@ -2,26 +2,21 @@ import hello from 'hellojs';
 
 import axios from 'axios';
 
-const SERVER_URL = 'http://localhost:8080';
-const FACEBOOK_CLIENT_ID = '700042113498773';
+import { FB_APPID, SERVER_URL } from '../config';
 
 hello.init({
-  facebook: FACEBOOK_CLIENT_ID,
+  facebook: FB_APPID,
 });
 
-
 export default () => {
-    return hello('facebook')
-      .login()
-      .then(response => {
-        return axios
-          .post(`${SERVER_URL}/auth/facebook`, {
-            socialToken: response.authResponse.access_token,
-          })
-          .then(response => response.data)
-      });
-  };
-
+  return hello('facebook').login().then(response => {
+    return axios
+      .post(`${SERVER_URL}/auth/facebook`, {
+        socialToken: response.authResponse.access_token,
+      })
+      .then(response => response.data);
+  });
+};
 
 // function init() {
 //   const userStr = localStorage.getItem('user');
--- client-web/src/config.js
+++ client-web/src/config.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = 'http://localhost:8080';
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL; //'http://localhost:8080';
+export const FB_APPID = process.env.REACT_APP_FB_APPID;
