e8d973d0038bc90686053b09ed7d14fc8b175b5d
chiptus <chiptus@gmail.com>
2017-03-21 13:58:54 +0200

    support dev/prod in config

--- client-web/src/config.js
+++ client-web/src/config.js
@@ -1,2 +1,8 @@
-export const SERVER_URL = process.env.REACT_APP_SERVER_URL; //'http://localhost:8080';
-export const FB_APPID = process.env.REACT_APP_FB_APPID;
+export const SERVER_URL = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_SERVER_URL
+  : process.env.REACT_APP_SERVER_URL_DEV;
+export const FB_APPID = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_FB_APPID
+  : process.env.REACT_APP_FB_APPID_DEV;
+
+console.log('config', SERVER_URL, FB_APPID);
